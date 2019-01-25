ce3107d7ae5488582983a9c5f254d0abd4de1cce
Michael Devine <2323011D@student.gla.ac.uk>
2019-01-25 12:55:08 +0000

    first formating

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -26,6 +26,7 @@ SECRET_KEY = 'mu^(w+y+le^izepq9e&na9&i1%7&tdo(z#rmr#rckt2ov$av!4'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Application definition
@@ -38,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rango',
-]
+]
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -55,7 +57,7 @@ ROOT_URLCONF = 'tango_with_django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [TEMPLATE_DIR,],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
